2f1ca9a65b4baf3dc05f73015e83ff03db51cea5
D0dzs <51715950+D0dzs@users.noreply.github.com>
2022-08-04 11:07:05 +0200

    hotfix

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
-import absoluteUrl from "next-absolute-url";
-const origin = absoluteUrl().host;
-
 export const config = {
     title: "Dodzs.dev",
     author: "Dodzs",
     description: "I'm a front-end developer base in Hungary! I enjoy building webs using Tailwind CSS, Next.js, and other stuffs.",
     theme_color: "#909090",
     type: "website",
-    url: origin,
+    url: "Dodzs.dev",
     keywords: "developer, front-end, web, nextjs, dodzs, magyar, hungarian, portfolio",
     smoothTransition: true,
 }
