3b5573cdb343d301585b0fccb148c1029d872eb5
D0dzs <51715950+D0dzs@users.noreply.github.com>
2022-08-06 19:55:14 +0200

    premid szar

--- src/pages/discord.tsx
+++ src/pages/discord.tsx
@@ -26,39 +26,89 @@ export default function Discord() {
         </p>
         <section className="flex flex-col gap-8 select-none m-8 md:flex-row flex-wrap md:w-[1120px] flex-auto justify-center">
           {data?.activities.map((val, index) => {
-            if (val?.assets?.large_text.match("PreMiD")) {
-              return (
-                <div className={boxStyle} key={index}>
-                  <div>
-                    <img
-                      src={`https://cdn.discordapp.com/app-assets/${val?.application_id}/${val?.assets?.large_image}.png`}
-                      className="rounded-md"
-                      width={76}
-                      height={76}
-                    />
-                  </div>
-                  <div className="flex flex-col items-start justify-center ml-4">
-                    <a
-                      href={`https://www.twitch.tv/${val?.state.toLocaleLowerCase()}`}
-                      target={"_blank"}
-                      rel="noreferrer"
-                      className="hover:text-[#9b65ff] transition-all duration-150 ease-in"
-                    >
-                      <p className="font-semibold self-left flex">
-                        {val?.name}{" "}
-                        <span>
-                          <CgExternal size={24} />
-                        </span>
-                      </p>
-                    </a>
-                    <p className="text-[0.9rem] hidden md:block">
-                      {val?.details}
-                    </p>
-                    <p className="text-[0.9rem]">{val?.state}</p>
-                  </div>
-                </div>
-              );
-            }
+            if (val?.assets?.large_text.match("PreMiD")) return;
+            // {
+            //   switch (val?.name.toLowerCase()) {
+            //     case "twitch":
+            //       return (
+            //         <div className={boxStyle} key={index}>
+            //           <div>
+            //             <img
+            //               src={`https://cdn.discordapp.com/app-assets/${val?.application_id}/${val?.assets?.large_image}.png`}
+            //               className="rounded-md"
+            //               width={76}
+            //               height={76}
+            //             />
+            //           </div>
+            //           <div className="flex flex-col items-start justify-center ml-4">
+            //             <a
+            //               href={`https://www.twitch.tv/${val?.state.toLocaleLowerCase()}`}
+            //               target={"_blank"}
+            //               rel="noreferrer"
+            //               className="hover:text-[#9b65ff] transition-all duration-150 ease-in"
+            //             >
+            //               <p className="font-semibold self-left flex">
+            //                 {val?.name}{" "}
+            //                 <span>
+            //                   <CgExternal size={24} />
+            //                 </span>
+            //               </p>
+            //             </a>
+            //             <p className="text-[0.9rem] hidden md:block">
+            //               {val?.details}
+            //             </p>
+            //             <p className="text-[0.9rem]">{val?.state}</p>
+            //           </div>
+            //         </div>
+            //       );
+            //     case "npm package registry":
+            //       return (
+            //         <div className={boxStyle} key={index}>
+            //           <div>
+            //             <img
+            //               src={`https://cdn.discordapp.com/app-assets/${val?.application_id}/${val?.assets?.large_image}.png`}
+            //               className="rounded-md"
+            //               width={76}
+            //               height={76}
+            //             />
+            //           </div>
+            //           <div className="flex flex-col items-start justify-center ml-4">
+            //             <p className="font-semibold self-left flex">
+            //               {val?.name}{" "}
+            //               <span>
+            //                 <CgExternal size={24} />
+            //               </span>
+            //             </p>
+            //             <p className="text-[0.9rem] hidden md:block">
+            //               {val?.details}
+            //             </p>
+            //             <p className="text-[0.9rem]">{val?.state}</p>
+            //           </div>
+            //         </div>
+            //       );
+            //     default:
+            //       return (
+            //         <div className={boxStyle} key={index}>
+            //           <div>
+            //             <img
+            //               src={`${val?.assets?.large_image.replace(
+            //                 /.*https/g,
+            //                 "https:/"
+            //               )}`}
+            //               className="rounded-md"
+            //               width={76}
+            //               height={76}
+            //             />
+            //           </div>
+            //           <div className="flex flex-col items-start justify-center ml-4">
+            //             <p className="font-semibold self-left">{val?.name}</p>
+            //             <p className="text-[0.9rem]">{val?.details}</p>
+            //             <p className="text-[0.9rem]">{val?.state}</p>
+            //           </div>
+            //         </div>
+            //       );
+            //   }
+            // }
 
             if (val?.name.toLocaleLowerCase() === "spotify") {
               return (
